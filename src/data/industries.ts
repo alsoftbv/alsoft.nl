3650891efcd0f5ebe3265ef00d55b92913a0db6f
Begüm Bakan <begumbakan2005@gmail.com>
2025-10-03 17:14:47 +0300

    Add services and industries page

---
+++ src/data/industries.ts
@@ -0,0 +1,22 @@
+const industries = [
+  {
+    id: "fintech",
+    title: "FinTech",
+    description: "Description could go here",
+    image: "/placeholder.jpg",
+  },
+  {
+    id: "manufacturing",
+    title: "Manufacturing",
+    description: "Description could go here",
+    image: "/placeholder.jpg",
+  },
+  {
+    id: "automotive",
+    title: "Automotive",
+    description: "Description could go here",
+    image: "/placeholder.jpg",
+  },
+];
+
+export default industries;
---
+++ src/data/services.ts
@@ -0,0 +1,22 @@
+export const services = [
+  {
+    id: 'rtos',
+    title: 'RTOS',
+    description: 'Description could go here',
+    image: '/placeholder.jpg',
+  },
+  {
+    id: 'linux',
+    title: 'Linux',
+    description: 'Description could go here',
+    image: '/placeholder.jpg',
+  },
+  {
+    id: 'iot',
+    title: 'IoT',
+    description: 'Description could go here',
+    image: '/placeholder.jpg',
+  },
+];
+
+export default services;
