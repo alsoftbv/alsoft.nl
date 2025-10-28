abcc0d10cf316005d4e1a259f154a66288ca3596
Begüm Bakan <begumbakan2005@gmail.com>
2025-10-28 18:20:53 +0300

    change img components to Astro Image components

--- src/data/industries.ts
+++ src/data/industries.ts
@@ -1,24 +1,28 @@
+import fintechImage from "../images/industries/fintech.jpg";
+import manufacturingImage from "../images/industries/manufacturing.jpg";
+import automotiveImage from "../images/industries/automotive.jpg";
+
 const industries = [
   {
     id: "fintech",
     title: "FinTech",
     description: "Description could go here",
     longDescription: "Longer description could go here for FinTech industry.",
-    image: "/industries/fintech.jpg",
+    image: fintechImage,
   },
   {
     id: "manufacturing",
     title: "Manufacturing",
     description: "Description could go here",
     longDescription: "Longer description could go here for Manufacturing industry.",
-    image: "/industries/manufacturing.jpg",
+    image: manufacturingImage,
   },
   {
     id: "automotive",
     title: "Automotive",
     description: "Description could go here",
     longDescription: "Longer description could go here for Automotive industry.",
-    image: "/industries/automotive.jpg",
+    image: automotiveImage,
   },
 ];
 
--- src/data/services.ts
+++ src/data/services.ts
@@ -1,24 +1,28 @@
+import rtosImage from "../images/services/rtos.jpg";
+import linuxImage from "../images/services/linux.jpg";
+import iotImage from "../images/services/iot.jpg";
+
 export const services = [
   {
     id: 'rtos',
     title: 'RTOS',
     description: 'Description could go here',
     longDescription: 'Longer description could go here for RTOS service.',
-    image: '/services/rtos.jpg',
+    image: rtosImage,
   },
   {
     id: 'linux',
     title: 'Linux',
     description: 'Description could go here',
     longDescription: 'Longer description could go here for Linux service.',
-    image: '/services/linux.jpg',
+    image: linuxImage,
   },
   {
     id: 'iot',
     title: 'IoT',
     description: 'Description could go here',
     longDescription: 'Longer description could go here for IoT service.',
-    image: '/services/iot.jpg',
+    image: iotImage,
   },
 ];
 
