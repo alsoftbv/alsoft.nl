26a22523b9327b90a68ae0f65c38d63a9dc9f114
Altuğ Bakan <mail@alt.ug>
2025-11-01 19:39:24 +0300

    deploy website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alsoft.nl",
   scopedStyleStrategy: "class",
   prefetch: true,
   integrations: [sitemap()],
