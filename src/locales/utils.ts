47797695245cfd629ae669522a3c7618e55ec91b
Altuğ Bakan <mail@alt.ug>
2025-12-28 22:07:41 +0100

    simplified index page translations

--- src/locales/utils.ts
+++ src/locales/utils.ts
@@ -1,6 +1,7 @@
 import en from "@locales/en.json";
 import nl from "@locales/nl.json";
 import type { GetStaticPathsItem } from "astro";
+import { getRelativeLocaleUrl } from "astro:i18n";
 
 export function getTranslationPaths(): GetStaticPathsItem[] {
   return [{ params: { lang: undefined } }, { params: { lang: "nl" } }];
@@ -8,4 +9,11 @@ export function getTranslationPaths(): GetStaticPathsItem[] {
 
 export function useTranslations(lang: string | undefined | number) {
   return lang === "nl" ? nl : en;
+}
+
+export function getPath(
+  lang: string | number | undefined,
+  path: string
+): string {
+  return getRelativeLocaleUrl(lang === "nl" ? "nl" : "en", path);
 }
