0012735f320b9bdfede3d8861a3814d21a8aacb7
Altuğ Bakan <mail@alt.ug>
2025-12-29 20:28:08 +0100

    simplified blog index page

--- src/locales/utils.ts
+++ src/locales/utils.ts
@@ -2,7 +2,13 @@ import { tools } from "@data/tools";
 import en from "@locales/en.json";
 import nl from "@locales/nl.json";
 import type { GetStaticPathsItem } from "astro";
+import { getCollection, type CollectionEntry } from "astro:content";
 import { getRelativeLocaleUrl } from "astro:i18n";
+
+export interface ToolInformation {
+  path: string;
+  tool: { title: string; description: string };
+}
 
 export function getTranslationPaths(): GetStaticPathsItem[] {
   return [{ params: { lang: undefined } }, { params: { lang: "nl" } }];
@@ -23,7 +29,9 @@ export function getPath(
   return getRelativeLocaleUrl(getLanguage(lang), path);
 }
 
-export function getLocalizedTools(lang: string | undefined | number) {
+export function getLocalizedTools(
+  lang: string | undefined | number
+): ToolInformation[] {
   return Object.entries(tools)
     .filter(([path]) => {
       return lang === "nl" ? path.startsWith("nl/") : !path.startsWith("nl/");
@@ -31,5 +39,22 @@ export function getLocalizedTools(lang: string | undefined | number) {
     .map(([path, tool]) => ({
       path: `/${path}`,
       tool,
+    }));
+}
+
+export async function getLocalizedPosts(
+  lang: string | undefined | number
+): Promise<CollectionEntry<"blog">[]> {
+  return (await getCollection("blog"))
+    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
+    .filter((p) => {
+      return lang === "nl" ? p.slug.startsWith("nl") : !p.slug.startsWith("nl");
+    })
+    .map((p) => ({
+      ...p,
+      slug:
+        lang === "nl"
+          ? `/${p.slug.replace("nl/", "nl/blog/")}`
+          : `/blog/${p.slug}`,
     }));
 }
