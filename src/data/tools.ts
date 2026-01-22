81407022a8677f4b000cd41fb8f4bc9515be7f1d
Altuğ Bakan <mail@alt.ug>
2026-01-22 16:08:27 +0100

    add sFONT to tools

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -2,9 +2,17 @@ import en from "@locales/en.json";
 import nl from "@locales/nl.json";
 
 export const tools = {
+  "tools/sfont": {
+    title: en.sfont.title,
+    description: en.sfont.description,
+  },
   "tools/websocket": {
     title: en.websocket.title,
     description: en.websocket.description,
+  },
+  "nl/tools/sfont": {
+    title: nl.sfont.title,
+    description: nl.sfont.description,
   },
   "nl/tools/websocket": {
     title: nl.websocket.title,
